eb5271bc38cc3574704593cb6d0a4faa4ab1c9ba
CJcool06 <cjcool06@yahoo.com.au>
2022-03-03 12:20:15 +1030

    Better logic on when to re-localise

--- controllers/LocalisationController.py
+++ controllers/LocalisationController.py
@@ -2,6 +2,7 @@ import rospy
 from std_msgs.msg import String, Bool
 from geometry_msgs.msg import Pose
 from datetime import datetime
+import json
 
 class LocalisationController():
     
@@ -12,6 +13,7 @@ class LocalisationController():
         self._last_re_localisation = None
         self._pose = None
         self._old_pose = None
+        self._last_detections = None
         
         # Subscribers
         rospy.Subscriber('tag_detector/detections', String, self._handle_detections)
@@ -33,8 +35,12 @@ class LocalisationController():
         
         if self._last_re_localisation == None:
             return True
-        else:
-            return (datetime.now() - self._last_re_localisation).total_seconds() > self._min_re_localisation_interval
+        elif self._tag_detected:
+            err_x, err_y = self._error_from_centre()
+            print(f'X: {err_x}, Y: {err_y}')
+            if err_x <= 0.3 and err_y <= 0.2:
+                return (datetime.now() - self._last_re_localisation).total_seconds() > self._min_re_localisation_interval
+        return False
     
     def re_localise(self):
         """
@@ -47,10 +53,29 @@ class LocalisationController():
         self._old_pose = self._pose                 # Save current pose.
         self._pose = None                           # Reset current pose.
         self.re_loc_publisher.publish(True)         # Signal the pose wrapper to re-localise.
+        print('Published.')
         while self._pose == None:                   # Wait for a reply from the wrapper.
             rospy.sleep(1/20)
         self._last_re_localisation = datetime.now() # Remember current time
         return True
+    
+    def _error_from_centre(self):
+        """
+        The percentage (0-1) of pixels that the best tag is off-centre for both x and y.
+        
+        For example, if the image is 1000 pixels wide and the tag's centre is at pixel 300
+        then we consider it 200 pixels off-centre, thus 200/(1000/2) = 0.4 = 40% error from the centre.
+        """
+        
+        if self._last_detections == None:
+            return 1
+        
+        best_x = 10000
+        best_y = 10000
+        for d in self._last_detections:
+            best_x = min(best_x, abs((d['image-dimensions'][1]//2) - d['center'][0]))
+            best_y = min(best_y, abs((d['image-dimensions'][0]//2) - d['center'][1]))
+        return best_x / (d['image-dimensions'][1] / 2), best_y / (d['image-dimensions'][0] / 2)
     
     def _handle_detections(self, data):
         """
@@ -59,8 +84,10 @@ class LocalisationController():
         
         if data.data == 'None':
             self._tag_detected = False
+            self._last_detections = None
         else:
             self._tag_detected = True
+            self._last_detections = json.loads(data.data)
             
     def _handle_new_pose(self, data):
         """
